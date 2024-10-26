68e8a6f4983c19212b3f853dd3c61238296d2080
valencsoares <pvcampossoares558@gmail.com>
2024-10-26 18:51:57 -0400

    Arrumado o nome do id a ser chamado do index

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Somar = function () {
-    let num1 = document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value
+    let num1 = document.getElementById("sum1").value
+    let num2 = document.getElementById("sum2").value
     let saida = document.getElementById("saida")
 
     let soma = parseFloat(num1) + parseFloat(num2)
